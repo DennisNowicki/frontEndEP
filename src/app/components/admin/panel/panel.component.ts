b6805e33d6a32aa3415a1bfeb3298e00ba35d455
Brecht <Brecht_leben@hotmail.com>
2020-02-16 15:20:18 +0100

    added admin panel content

--- src/app/components/admin/panel/panel.component.ts
+++ src/app/components/admin/panel/panel.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Userdata } from '../../../data/userdata/userdata.model';
 import { UserdataService} from '../../../data/userdata/userdata.service';
+import { Components } from '../../../data/components/components.model';
+import { ComponentsService} from '../../../data/components/components.service';
 import { NgModel } from '@angular/forms';
 
 @Component({
@@ -12,11 +14,13 @@ import { NgModel } from '@angular/forms';
 export class PanelComponent implements OnInit {
 
   public userdata$: Observable<Userdata[]>;
+  public components$: Observable<Components[]>;
 
-  constructor(private UserdataService: UserdataService) { }
+  constructor(private UserdataService: UserdataService, private ComponentService: ComponentsService) { }
 
   ngOnInit() {
     this.userdata$ = this.UserdataService.getUserdata();
+    this.components$ = this.ComponentService.getComponents();
   }
 
 }
--- src/app/data/userdata/userdata.model.ts
+++ src/app/data/userdata/userdata.model.ts
@@ -4,24 +4,18 @@ export class Userdata {
       public applicationType: string,
       public applicationDescription: string,
       public pagesCount: number,
-      public pagesNames: {
-        pageName:string
-      },
-      public adminPanel: boolean,
-      public calendar: boolean,
-      public shoppingCart: boolean,
-      public chatbox: boolean,
-      public database: boolean,
-      public geolocation: boolean,
-      public multiLanguage: boolean,
-      public loginSystem: boolean,
-      public mediaSupport: boolean,
-      public notification: boolean,
-      public paymentService: boolean,
-      public statistics: boolean,
-      public techSupport: boolean,
-      public fileUpload: boolean,
-      public userExperience: boolean,
+      public pagesNames: [ 
+        {pageName:string}
+      ],
+      public component: [ 
+        {
+          id: void,
+          name: string,
+          src: string,
+          fee: number,
+          needed: boolean
+        }
+      ],
       public name: string,
       public email: string,
       public message: string,
